15d430a47eeb39837f7596539a01ba03ed82d5c5
Hidekazoo <tanakahidek@gmail.com>
2022-12-26 14:49:03 +0000

    Update release to output generated at d9f4dcd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "32dfd424fee9047bbf0bea02a90556b9",
+  "main.dart.js": "c82b3752687123b3ef84c8396d17ba63",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
@@ -20,8 +20,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/NOTICES": "60e2cd24405cdcef66314cd31b3bb49b",
-"index.html": "fa914c2e99247c3a4d316807bc6c1882",
-"/": "fa914c2e99247c3a4d316807bc6c1882",
+"index.html": "61aa233d55fdc9926d22d2715c3cf844",
+"/": "61aa233d55fdc9926d22d2715c3cf844",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "259ca6a9b74b7b023310958017c786f3"
 };
